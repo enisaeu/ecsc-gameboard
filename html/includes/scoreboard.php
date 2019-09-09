0f8fd226ec6111fda9076d22eddd385a77ce8e29
Miroslav Stampar <miroslav.stampar@gmail.com>
2019-09-09 15:59:51 +0200

    Standalone scoreboard (/scoreboard/)

---
+++ html/includes/scoreboard.php
@@ -0,0 +1,26 @@
+<?php
+    include_once("rankings.php");
+?>
+<script>
+    $(document).ready(function() {
+        var element = $("#line_momentum");
+        element.detach();
+        $('body').append(element);
+        $("#main_container").remove();
+        $("#line_momentum").css({'height': ($(window).height()) + 'px'});
+
+        // Note: dirty patch for proper resizing / redrawing
+        var $_ = function() {
+            if (chart == null)
+                setTimeout($_, 10);
+            else
+                chart.render();
+        };
+        setTimeout($_, 10);
+
+        setTimeout(function() {
+            location.reload();
+        }, SCOREBOARD_PAGE_RELOAD);
+
+    });
+</script>
